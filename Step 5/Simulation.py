6d8008b2b91b2cd08b45e0fb7cc84f0ba8dd9019
tessamg18 <60265321+tessamg18@users.noreply.github.com>
2020-03-13 11:37:58 -0400

    Update Simulation.py

--- Step 5/Simulation.py
+++ Step 5/Simulation.py
@@ -6,17 +6,24 @@ Name(s): Vicky Yang & Tessa Grasseschi
 
 # IMPORT STATEMENTS
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sig
+import statistics
+
+#global variables
+num_samples = 100000
 
 # CUSTOM FUNCTIONS
-def update_system(L,acc,vel,pos):
-    dt = 0.1
-    accNext = ((-9.8)/L)*(np.sin(pos))
+def update_system(L,acc,vel,pos,t):
+    dt = 0.001
+    tNext=t+dt
     posNext = pos+vel*dt
+    accNext = ((-9.8)/L)*(np.sin((posNext*np.pi)/180))
     velNext = vel+acc*dt
-    return posNext, velNext, accNext, dt
+    return posNext, velNext, accNext, tNext
 
 # MAIN SCRIPT
-
+sim_state = np.zeros((num_samples,4))
 # Initializing
 # make sure in radians
 # pos = angle
@@ -24,20 +31,54 @@ def update_system(L,acc,vel,pos):
 
 #L = [0.2540, 0.3048, 0.3556, 0.4064, 0.4572]
 L = 0.2540
-t0 = 0
-acc0 = 0
-vel0 = 0
-pos0 = 45
 
-time = [t0]
-acc = [acc0]
-vel = [vel0]
-pos = [pos0]
+sim_state[0,0] = 0 #time
+sim_state[0,1] = 0 #accel
+sim_state[0,2] = 0 #v
+sim_state[0,3] = 45 #pos
 
+for i in range(num_samples-1):
+     posNext, velNext, accNext, tNext = update_system(0.2540, sim_state[i,1], sim_state[i,2], sim_state[i,3],sim_state[i,0])
+     sim_state[i+1,0] = tNext #time
+     sim_state[i+1,1] = accNext #accel
+     sim_state[i+1,2] = velNext #v
+     sim_state[i+1,3] = posNext #pos
+     
+plt.plot(sim_state[:,0],sim_state[:,3])
+plt.show()
 #x = update_system(L,acc0,vel0,pos0)
-#i = 1
+#i = i+1
 #for element in time:
 #    if element < 20:
-#        posNext, velNext = update_system(0.2540, acc[i], vel[i-1], pos[i-1])
+#        posNext, velNext = update_system(0.2540, acc[i-1], vel[i-1], pos[i-1])
 #        pos.append(posNext)
 #        vel.append(velNext)
+
+peak_10 = sig.find_peaks(sim_state[:,3], distance=11)
+peak_10_times = []
+for index in peak_10[0]:
+   time = sim_state[index,0]
+   peak_10_times.append(time)
+period_10 = []
+for i in range(len(peak_10_times)-1):
+    time_difference = peak_10_times[i+1] - peak_10_times[i]
+    period_10.append(time_difference)
+peak_10_mean = statistics.mean(period_10)
+
+plt.plot(sim_state[:,0], sim_state[:,3], 'r-', sim_state[peak_10[0],0], sim_state[peak_10[0],3], 'b.')
+plt.title('Theta vs. Time for 10 inch Pendulum with Peaks')
+plt.ylabel('Theta (degrees)')
+plt.xlabel('Time (s)')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
